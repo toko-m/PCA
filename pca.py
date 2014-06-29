a74e16587ba67dce9ab9af950a0cc88a4df3af42
Toko Miyamori <tokom@ult.sakura.ne.jp>
2014-06-29 21:41:04 +0900

    change repository name as PCA

---
+++ pca.py
@@ -0,0 +1,24 @@
+import json
+
+input_data = {}
+
+def mva_main():
+	print "hoge"
+
+class mva():
+	def 
+
+class InputData():
+	def __init__():
+		print "huhuhu"
+
+	def create_data_from_json(input_json):
+		json_data = json.load(input_json)
+		
+	def create_data_from_csv(input_file):
+		f = open(input_file)
+		csv_data = json.load(f)
+
+
+if __name__ == '__main__':
+	mva_main()
