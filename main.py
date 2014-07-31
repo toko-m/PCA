fa642b3e4096b95db8aad215939f2776e7ee7943
Toko Miyamori <tokom@ult.sakura.ne.jp>
2014-08-01 00:17:47 +0900

    Checked cgi operation

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+#!/usr/bin/python
+
 import cgi
 import cgitb
 import pca
+import os
 
-cgitb.enable(display=0, logdir="./log")
+cgitb.enable()
+
+
+if os.environ['REQUEST_METHOD'] != "POST":
+	raise Exception ('Request must be post.')
 
 rcv_data = cgi.FieldStorage()
 PCA_instance = None
 
-if rcv_data is not None:
-	PCA_instance = rcv_data.getvalue("PCA_type")
-	input_data = rcv_data.getvalue("PCA_input")
+if rcv_data is None:
+	raise Exception ('Receive data is none.')
+
+PCA_instance = rcv_data.getvalue("PCA_type")
+input_data = rcv_data.getvalue("PCA_input")
+
+print "Content-type:text/html\n\n"
+print "hogee"
